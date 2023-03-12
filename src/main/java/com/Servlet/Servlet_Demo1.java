3e9de2004df132dbfdfea0df9844cd59bd743e0e
zzhang <zzhang2293@outlook.com>
2023-03-12 16:16:48 -0500

    demo1

--- src/main/java/com/Servlet/Servlet_Demo1.java
+++ src/main/java/com/Servlet/Servlet_Demo1.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
-@WebServlet("/demo1")
+@WebServlet(urlPatterns = "/demo1", loadOnStartup = 1)
 public class Servlet_Demo1 implements Servlet {
 
 
@@ -12,8 +12,21 @@ public class Servlet_Demo1 implements Servlet {
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println("Hello World");
     }
+
+    /**
+     * INITIALIZE
+     * 1. default : first servlet visited
+     *      *loadOnStartup:
+     * 2. just one time
+     * @param servletConfig
+     * @throws ServletException
+     */
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init");
+    }
+    @Override
+    public void destroy() {
 
     }
 
@@ -28,8 +41,5 @@ public class Servlet_Demo1 implements Servlet {
         return null;
     }
 
-    @Override
-    public void destroy() {
 
-    }
 }
